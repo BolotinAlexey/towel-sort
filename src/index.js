5bff4924b0d90855945c692b477d5d84a803c9e5
Oleksii Bolotin <62134373+BolotinAlexey@users.noreply.github.com>
2023-08-07 02:46:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-module.exports = function towelSort(matrix) {
-    return matrix
-        ? matrix
-              .map((el, i) => (i % 2 ? [...el].reverse() : el))
-              .reduce((a, b) => [...a, ...b], [])
-        : [];
-};
+
